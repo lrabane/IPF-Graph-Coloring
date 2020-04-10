a8d36814543316328541a0297bd27e77e4895f49
Lucas Rabane <lucas@rabane.dev>
2020-04-10 21:48:16 +0200

    Add coloring.ml

---
+++ coloring.ml
@@ -0,0 +1,65 @@
+(* 1 *)
+
+module StringSet = Set.Make(String)
+module StringMap = Map.Make(String)
+
+type graph = StringSet.t StringMap.t
+
+(* 2 *)
+
+let add_edge u v g =
+  let add_oriented_edge v1 v2 g =
+    let s =
+      try
+        StringMap.find v1 g
+      with
+        Not_found -> StringSet.empty
+    in
+    let s' = StringSet.add v2 s in
+    StringMap.add v1 s' g in
+  let g = add_oriented_edge u v g in
+  add_oriented_edge v u g
+
+(* 3 *)
+
+let remove_vertex u g =
+  let g = StringMap.remove u g in
+  
+  StringMap.fold 
+    (fun k d a -> 
+      let s = StringSet.remove u d in
+      StringMap.add k s a
+    )
+    g
+    StringMap.empty
+
+(* 4 *)    
+
+module Int = struct
+  type t = int
+  let compare = fun x y -> x - y
+end
+  
+module IntSet = Set.Make(Int)
+
+(* 5 *)
+
+let rec range a b =
+  if a = b then IntSet.singleton a
+  else IntSet.add a (range (a+1) b)
+
+let color_set n =
+  range 1 n
+
+
+
+(*
+let my_graph =
+  let g = add_edge "f" "d" StringMap.empty in
+  let g = add_edge "f" "e" g in
+  let g = add_edge "d" "e" g in
+  let g = add_edge "d" "b" g in 
+  let g = add_edge "d" "a" g in
+  let g = add_edge "e" "a" g in
+  add_edge "a" "c" g
+*)
