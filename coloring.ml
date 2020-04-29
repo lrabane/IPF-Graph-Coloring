09981ee56c67555827d21cfac3e59a0b3bf8dbc7
MARWAN <mwgg@sfr.fr>
2020-04-29 23:03:46 +0200

    Question 12 - color function - WIP

--- coloring.ml
+++ coloring.ml
@@ -82,6 +82,30 @@ let rec try_first f s =
 
 type coloring = int StringMap.t
 
+(* 12 - WIP (ne fonctionne pas encore) *)
+let rec color g c =
+  if StringMap.is_empty g = false then (* Si g n'est pas vide alors faire le traitement suivant *)
+  let (s, sv) = StringMap.choose g in (* On choisi un noeud dans le graph g *)
+  let _ = IntSet.map (fun color_c -> (* Boucle dans les couleurs disponibles de s (s : le noeud sélectionné dans choose) *)
+  let c' = StringMap.fold(fun noeud_c value_noeud_c result-> (* On enlève la couleur color_c dans sv qui sont les voisins de s *)
+                          let element =
+                          try
+                          StringSet.find noeud_c sv
+                          with
+                          Not_found -> "" in
+                          if element <> "" then remove_color color_c noeud_c result else result) c c in
+  let g' = remove_vertex s g in (* On enlève s dans g' *)
+  let col =
+  try
+  StringMap.add s color_c (color g' c) (* Problème comment détecter si col a bien renvoyé une valeur *)
+  with
+  (Failed error_string) -> None
+  in
+  if col <> None then StringMap.add s color_c col else Printf.printf "Next\n"
+  ) (StringMap.find s c) in 
+  raise (Failed "Erreur fin de parcours des couleurs") (* Erreur après la bouble sur toutes couleurs disponibles *)
+  else 
+  StringMap.empty (* Si le graph g est vide on retourne une StringMap vide qui sera le fond de la fonction récursive *)
 
 
 (*
